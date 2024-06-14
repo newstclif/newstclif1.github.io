5fec67c144cf506ac6914b4de9a537c55d368c1b
newstclif <55538723+newstclif@users.noreply.github.com>
2024-06-14 06:08:57 +0300

    Add files via upload

---
+++ Vis.1/keyshot-configurator-imagemapping.js
@@ -0,0 +1,171 @@
+imagemappingdata =
+[
+    {
+        "image": "Vis.1.1.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 552584696
+    },
+    {
+        "image": "Vis.1.2.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 552584696
+    },
+    {
+        "image": "Vis.1.3.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 552584696
+    },
+    {
+        "image": "Vis.1.4.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 552584696
+    },
+    {
+        "image": "Vis.1.5.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 552584531
+    },
+    {
+        "image": "Vis.1.6.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 552584531
+    },
+    {
+        "image": "Vis.1.7.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 552584531
+    },
+    {
+        "image": "Vis.1.8.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 552584531
+    },
+    {
+        "image": "Vis.1.9.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 552584862
+    },
+    {
+        "image": "Vis.1.10.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 552584862
+    },
+    {
+        "image": "Vis.1.11.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 552584862
+    },
+    {
+        "image": "Vis.1.12.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 552584862
+    },
+    {
+        "image": "Vis.1.13.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 552584863
+    },
+    {
+        "image": "Vis.1.14.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 552584863
+    },
+    {
+        "image": "Vis.1.15.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 552584863
+    },
+    {
+        "image": "Vis.1.16.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 552584863
+    },
+    {
+        "image": "Vis.1.17.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 696887045
+    },
+    {
+        "image": "Vis.1.18.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 696887045
+    },
+    {
+        "image": "Vis.1.19.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 696887045
+    },
+    {
+        "image": "Vis.1.20.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 696887045
+    },
+    {
+        "image": "Vis.1.21.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 696887047
+    },
+    {
+        "image": "Vis.1.22.png",
+        "modelsets": [
+            552584532
+        ],
+        "studio": 696887047
+    },
+    {
+        "image": "Vis.1.23.png",
+        "modelsets": [
+            552584698
+        ],
+        "studio": 696887047
+    },
+    {
+        "image": "Vis.1.24.png",
+        "modelsets": [
+            696888945
+        ],
+        "studio": 696887047
+    }
+];
---
+++ Vis.1/keyshot-configurator-modeldata.js
@@ -0,0 +1,60 @@
+model =
+{
+    "materialgroups": [],
+    "products": [
+        {
+            "checked": true,
+            "icon": "1.png",
+            "modelset": 1005,
+            "name": "1"
+        },
+        {
+            "icon": "2.png",
+            "modelset": 552584532,
+            "name": "2"
+        },
+        {
+            "icon": "3.png",
+            "modelset": 552584698,
+            "name": "3"
+        },
+        {
+            "icon": "4.png",
+            "modelset": 696888945,
+            "name": "4"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "studio%201.png",
+            "id": 552584531,
+            "name": "Studio 1"
+        },
+        {
+            "icon": "studio%202.png",
+            "id": 552584696,
+            "name": "Studio 2"
+        },
+        {
+            "icon": "studio%203.png",
+            "id": 552584862,
+            "name": "Studio 3"
+        },
+        {
+            "icon": "studio%204.png",
+            "id": 552584863,
+            "name": "Studio 4"
+        },
+        {
+            "icon": "studio%205.png",
+            "id": 696887045,
+            "name": "Studio 5"
+        },
+        {
+            "checked": true,
+            "icon": "studio%206.png",
+            "id": 696887047,
+            "name": "Studio 6"
+        }
+    ]
+};
